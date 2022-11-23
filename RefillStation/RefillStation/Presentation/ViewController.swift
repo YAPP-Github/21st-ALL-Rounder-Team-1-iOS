48361c21783c8ec1e81199e3f27c4c469c092892
Neph <tony3322@naver.com>
2022-11-23 11:57:06 +0900

    [Chore] 기본 세팅 작업

---
+++ RefillStation/RefillStation/Presentation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RefillStation
+//
+//  Created by 천수현 on 2022/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
