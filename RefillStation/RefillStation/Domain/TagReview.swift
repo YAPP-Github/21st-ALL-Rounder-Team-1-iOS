b640241a036f9581b448f296132fd68d4a70a4f0
Neph <tony3322@naver.com>
2022-11-23 18:04:02 +0900

    [Feat] #1 TagReviewView, TagReview 생성

---
+++ RefillStation/RefillStation/Domain/TagReview.swift
@@ -0,0 +1,13 @@
+//
+//  TagReview.swift
+//  RefillStation
+//
+//  Created by 천수현 on 2022/11/23.
+//
+
+import Foundation
+
+struct TagReview {
+    let tagTitle: String
+    let voteCount: Int
+}
---
+++ RefillStation/RefillStation/Presentation/TagReviewView.swift
@@ -0,0 +1,13 @@
+//
+//  TagReviewView.swift
+//  RefillStation
+//
+//  Created by 천수현 on 2022/11/23.
+//
+
+import UIKit
+import SnapKit
+
+final class TagReviewView: UIView {
+
+}
