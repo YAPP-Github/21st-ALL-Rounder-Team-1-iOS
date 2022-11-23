f3728b70d5b6315b99bfba5d70572c7aaa8e1cf3
Neph <tony3322@naver.com>
2022-11-23 12:08:13 +0900

    [Chore] SwiftLint 세팅

--- RefillStation/RefillStation/Presentation/ViewController.swift
+++ RefillStation/RefillStation/Presentation/ViewController.swift
@@ -12,9 +12,5 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
