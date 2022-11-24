a63b0e22f977b340304b01d9f94c587b68e3021b
Neph <tony3322@naver.com>
2022-11-24 16:31:35 +0900

    [Feat] button 디테일 설정 및 mock데이터 생성

--- RefillStation/RefillStation/Presentation/StoreReviewViewController.swift
+++ RefillStation/RefillStation/Presentation/StoreReviewViewController.swift
@@ -13,6 +13,9 @@ final class StoreReviewViewController: UIViewController {
     private let moveToWritingReviewButton: UIButton = {
         let button = UIButton()
         button.setTitle("방문하셨다면 리뷰를 남겨주세요! ✏️", for: .normal)
+        button.setTitleColor(UIColor.black, for: .normal)
+        button.layer.cornerRadius = 5
+        button.backgroundColor = .lightGray
         return button
     }()
 
@@ -21,16 +24,45 @@ final class StoreReviewViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        initailizeViews()
+        layout()
+        addMoveToWritingReviewButtonTarget()
     }
 
     private func initailizeViews() {
         let mockTagReviewViewModel: TagReviewViewModel = {
             let viewModel = TagReviewViewModel()
+            viewModel.totalVoteCount = 10
+            viewModel.tagReviews = [
+                .init(tagTitle: "청결해요", voteCount: 5),
+                .init(tagTitle: "친절해요", voteCount: 6),
+                .init(tagTitle: "배고파요", voteCount: 7),
+                .init(tagTitle: "살려줘요", voteCount: 8)
+            ]
             return viewModel
         }()
 
         let mockDetailReviewViewModel: DetailReviewViewModel = {
             let viewModel = DetailReviewViewModel()
+            viewModel.detailReviews = [
+                .init(user: .init(name: "", profileImageURL: ""),
+                      writtenDate: Date(),
+                      imageURLs: [],
+                      description: "description"),
+                .init(user: .init(name: "", profileImageURL: ""),
+                      writtenDate: Date(),
+                      imageURLs: [],
+                      description: "description"),
+                .init(user: .init(name: "", profileImageURL: ""),
+                      writtenDate: Date(),
+                      imageURLs: [],
+                      description: "description"),
+                .init(user: .init(name: "", profileImageURL: ""),
+                      writtenDate: Date(),
+                      imageURLs: [],
+                      description: "description")
+            ]
             return viewModel
         }()
         tagReviewView = TagReviewView(viewModel: mockTagReviewViewModel)
