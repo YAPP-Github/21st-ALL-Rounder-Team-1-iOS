ebbf0ebf4d78589044d69f41be5ba31122d42552
kong <1018dbrud@gmail.com>
2022-11-24 21:50:15 +0900

    [Feat] #6 StoreDetailViewModel 제작

---
+++ RefillStation/RefillStation/Presentation/StoreDetailScene/ViewModel/StoreDetailViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  StoreDetailViewModel.swift
+//  RefillStation
+//
+//  Created by kong on 2022/11/24.
+//
+
+import Foundation
+
+final class StoreDetailViewModel {
+    var name: String = "알맹 상점"
+    var reviewCount: Int = 1018
+    var address: String = "서울시 성북구 삼선동"
+    var openState: Bool = true
+    var closeTime: String = "8:00"
+    var distance: Int = 5
+}
