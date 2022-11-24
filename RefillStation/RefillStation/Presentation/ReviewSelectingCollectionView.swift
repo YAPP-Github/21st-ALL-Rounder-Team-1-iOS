3b3723ec5f68f262894d2573a925d6af516beac1
Neph <tony3322@naver.com>
2022-11-24 13:23:13 +0900

    [Feat] #7 ReviewSelectingViewModel 제작, compositional layout제작

--- RefillStation/RefillStation/Presentation/ReviewSelectingCollectionView.swift
+++ RefillStation/RefillStation/Presentation/ReviewSelectingCollectionView.swift
@@ -10,12 +10,36 @@ import SnapKit
 
 final class ReviewSelectingCollectionView: UICollectionView {
 
+    var viewModel: ReviewSelectingViewModel!
+
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+        self.collectionViewLayout = compositionalLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func compositionalLayout() -> UICollectionViewCompositionalLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                              heightDimension: .fractionalHeight(1/4))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(3/5),
+                                               heightDimension: .fractionalHeight(1))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+
+        return UICollectionViewCompositionalLayout(section: section)
+    }
 }
+
+// MARK: - UICollectionViewDataSource
 
 extension ReviewSelectingCollectionView: UICollectionViewDataSource {
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
+        return viewModel.reviews.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
---
+++ RefillStation/RefillStation/Presentation/ReviewSelectingViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ReviewSelectingViewModel.swift
+//  RefillStation
+//
+//  Created by 천수현 on 2022/11/24.
+//
+
+import Foundation
+
+final class ReviewSelectingViewModel {
+
+    var reviews = [TagReviewView]()
+}
