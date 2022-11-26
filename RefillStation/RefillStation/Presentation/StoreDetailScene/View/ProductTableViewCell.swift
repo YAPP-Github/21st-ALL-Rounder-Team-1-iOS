c1921f3cdbb085bea76b63cd0fe033efbd206804
kong <1018dbrud@gmail.com>
2022-11-27 03:51:16 +0900

    [Feat] #12 ProductTableViewCell 생성

---
+++ RefillStation/RefillStation/Presentation/StoreDetailScene/View/ProductTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProductTableViewCell.swift
+//  RefillStation
+//
+//  Created by kong on 2022/11/27.
+//
+
+import UIKit
+import SnapKit
+
+final class ProductTableViewCell: UITableViewCell {
+
+}
