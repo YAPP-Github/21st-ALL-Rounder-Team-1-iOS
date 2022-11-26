6fda3562d502c2373b00e11e68995bf2d2c05321
Neph <tony3322@naver.com>
2022-11-27 07:46:00 +0900

    [Feat] #14 등록하기 버튼을 위한 셀 제작

---
+++ RefillStation/RefillStation/Presentation/ReviewWritingVC/reviewRegisterCell.swift
@@ -0,0 +1,37 @@
+//
+//  reviewRegisterCell.swift
+//  RefillStation
+//
+//  Created by 천수현 on 2022/11/27.
+//
+
+import UIKit
+import SnapKit
+
+final class reviewRegisterCell: UICollectionViewCell {
+
+    static let reuseIdentifier = "reviewRegisterCell"
+
+    private let registerButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("등록하기", for: .normal)
+        button.backgroundColor = .blue
+        button.layer.cornerRadius = 5
+        return button
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func layout() {
+        contentView.addSubview(registerButton)
+        registerButton.snp.makeConstraints { button in
+            button.edges.equalToSuperview()
+        }
+    }
+}
