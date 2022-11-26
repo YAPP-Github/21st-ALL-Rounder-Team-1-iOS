6cb9b291670ba78edcac4a046e118fffd4521c1f
Neph <tony3322@naver.com>
2022-11-26 13:12:44 +0900

    [Feat] #14 VoteTitleCell 제작

--- RefillStation/RefillStation/Presentation/StoreInfoCell.swift
+++ RefillStation/RefillStation/Presentation/StoreInfoCell.swift
@@ -28,7 +28,7 @@ final class StoreInfoCell: UICollectionViewCell {
     }
 
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
     }
 
     func setUpContents(storeName: String, storeLocationInfo: String) {
--- RefillStation/RefillStation/Presentation/VoteTitleCell.swift
+++ RefillStation/RefillStation/Presentation/VoteTitleCell.swift
@@ -10,13 +10,39 @@ import SnapKit
 
 final class VoteTitleCell: UICollectionViewCell {
 
-    static let reuseIdentifier = "oteTitleCell"
+    static let reuseIdentifier = "voteTitleCell"
+
+    private let voteTitleLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+
+    private let maximumVoteLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+        layout()
     }
 
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
+    }
+
+    private func layout() {
+        [voteTitleLabel, maximumVoteLabel].forEach {
+            contentView.addSubview($0)
+        }
+
+        voteTitleLabel.snp.makeConstraints { titleLabel in
+            titleLabel.leading.top.bottom.equalToSuperview().inset(5)
+        }
+
+        maximumVoteLabel.snp.makeConstraints { voteLabel in
+            voteLabel.leading.equalTo(voteTitleLabel.snp.trailing).inset(5)
+            voteLabel.trailing.top.bottom.equalToSuperview().inset(5)
+        }
     }
 }
