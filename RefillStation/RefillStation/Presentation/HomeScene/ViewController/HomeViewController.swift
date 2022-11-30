94342eb52900f20bfdc83a087596565a41daffdc
kong <1018dbrud@gmail.com>
2022-11-30 22:24:18 +0900

    [Feat] #24 HomeVC 생성 및 StoreCollectionView 추가

---
+++ RefillStation/RefillStation/Presentation/HomeScene/ViewController/HomeViewController.swift
@@ -0,0 +1,75 @@
+//
+//  StoreListViewController.swift
+//  RefillStation
+//
+//  Created by kong on 2022/11/30.
+//
+
+import UIKit
+import SnapKit
+
+final class HomeViewController: UIViewController {
+
+    // MARK: - UI Components
+    private var storeCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero,
+                                              collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.register(StoreCollectionViewCell.self,
+                                forCellWithReuseIdentifier: StoreCollectionViewCell.reuseIdentifier)
+        collectionView.showsVerticalScrollIndicator = false
+        return collectionView
+    }()
+
+    // MARK: - Properties
+    private let viewModel = HomeViewModel()
+
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        storeCollectionView.dataSource = self
+        storeCollectionView.delegate = self
+        layout()
+    }
+    // MARK: - Default Setting Methods
+
+    private func layout() {
+        [storeCollectionView].forEach { view.addSubview($0) }
+        storeCollectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
+extension HomeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.stores.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: StoreCollectionViewCell.reuseIdentifier,
+            for: indexPath) as? StoreCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+
+        let data = viewModel.stores[indexPath.row]
+        cell.render()
+        cell.setUpContents(image: data.imageURL,
+                           name: data.name,
+                           address: data.address,
+                           distance: data.distance,
+                           openState: data.openState,
+                           time: data.closeTime)
+        return cell
+    }
+}
+
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.bounds.width - 32, height: 267)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 12
+    }
+}
