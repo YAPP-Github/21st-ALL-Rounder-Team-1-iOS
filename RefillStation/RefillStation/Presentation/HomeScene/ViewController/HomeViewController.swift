6079add44733a9cf77c921118954e30b645313d6
kong <1018dbrud@gmail.com>
2022-12-03 15:51:13 +0900

    [Feat] #27 StoreCollectionView Header 추가

--- RefillStation/RefillStation/Presentation/HomeScene/ViewController/HomeViewController.swift
+++ RefillStation/RefillStation/Presentation/HomeScene/ViewController/HomeViewController.swift
@@ -52,6 +52,9 @@ final class HomeViewController: UIViewController {
                                               collectionViewLayout: layout)
         collectionView.register(StoreCollectionViewCell.self,
                                 forCellWithReuseIdentifier: StoreCollectionViewCell.reuseIdentifier)
+        collectionView.register(ApplyForRegionHeaderView.self,
+                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                                withReuseIdentifier: ApplyForRegionHeaderView.reuseIdentifier)
         collectionView.showsVerticalScrollIndicator = false
         return collectionView
     }()
@@ -64,6 +67,7 @@ final class HomeViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         storeCollectionView.dataSource = self
+        storeCollectionView.delegate = self
         layout()
     }
 
@@ -123,5 +127,24 @@ extension HomeViewController: UICollectionViewDataSource {
                            openState: data.openState,
                            time: data.closeTime)
         return cell
+    }
+}
+
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 16, left: 0, bottom: 0, right: 0)
+    }
+    func collectionView(_ collectionView: UICollectionView,
+                        viewForSupplementaryElementOfKind kind: String,
+                        at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
+                                                                     withReuseIdentifier: ApplyForRegionHeaderView.reuseIdentifier,
+                                                                     for: indexPath)
+        return header
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        let headerHeight: CGFloat = viewModel.isServiceRegion ? 0 : 325
+
+        return CGSize(width: view.frame.width, height: headerHeight)
     }
 }
