924c9e44582fd620610a3e7c16022d5ffd64eccb
kong <1018dbrud@gmail.com>
2022-12-04 00:01:20 +0900

    [Chore] #29 신청 버튼에 폰트 적용

--- RefillStation/RefillStation/Presentation/RequestRegionScene/ViewController/RequestRegionViewController.swift
+++ RefillStation/RefillStation/Presentation/RequestRegionScene/ViewController/RequestRegionViewController.swift
@@ -49,6 +49,7 @@ final class RequestRegionViewController: UIViewController {
         button.setTitleColor(Asset.Colors.gray4.color, for: .disabled)
         button.setTitleColor(.white, for: .normal)
         button.setTitle("신청하기", for: .normal)
+        button.titleLabel?.font = .font(style: .buttonLarge)
         button.layer.cornerRadius = 8
         button.backgroundColor = Asset.Colors.primary3.color
         return button
