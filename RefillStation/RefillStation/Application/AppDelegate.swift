7f4d7e1a3eda139efca5b7871e7bf65bf1f48a04
Neph <tony3322@naver.com>
2022-12-04 06:40:44 +0900

    [Feat] #35 navigationBar appearance 변경

--- RefillStation/RefillStation/Application/AppDelegate.swift
+++ RefillStation/RefillStation/Application/AppDelegate.swift
@@ -13,6 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .white
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = UINavigationController(rootViewController: HomeViewController())
         window?.makeKeyAndVisible()
