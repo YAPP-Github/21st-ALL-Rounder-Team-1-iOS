52a1237c2d5e4b4b3dfc7cf073b917880ae01860
kong <1018dbrud@gmail.com>
2022-12-03 19:51:54 +0900

    [Chore] #27 fatalError 삭제

--- RefillStation/RefillStation/Presentation/HomeScene/View/ApplyForRegionHeaderView.swift
+++ RefillStation/RefillStation/Presentation/HomeScene/View/ApplyForRegionHeaderView.swift
@@ -58,7 +58,7 @@ final class ApplyForRegionHeaderView: UICollectionReusableView {
         layout()
     }
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
     }
 
     // MARK: - Default Setting Methods
