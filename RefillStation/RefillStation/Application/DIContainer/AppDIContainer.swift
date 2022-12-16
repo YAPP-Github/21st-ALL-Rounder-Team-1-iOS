970cb9f2ed5f7f18ba92442c3c3d3f5466265a76
Neph <tony3322@naver.com>
2022-12-16 16:39:01 +0900

    [Feat] #38 AppDIContainer 추가 안올라간 것 커밋

---
+++ RefillStation/RefillStation/Application/DIContainer/AppDIContainer.swift
@@ -0,0 +1,18 @@
+//
+//  AppDIContainer.swift
+//  RefillStation
+//
+//  Created by 천수현 on 2022/12/16.
+//
+
+import Foundation
+
+final class AppDIContainer: DIContainer {
+//    func makeHomeSceneDIContainer() -> HomeSceneDIContainer {
+//        return HomeSceneDIContainer(coordinator: <#HomeSceneCoordinator#>)
+//    }
+//
+//    func makeOnboardingDIContainer() -> OnboardingDIContainer {
+//        return OnboardingDIContainer()
+//    }
+}
