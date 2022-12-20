dafc2bb5aa0b06aff71cc5c3cfea0899c2c65ccc
Neph <tony3322@naver.com>
2022-12-20 13:49:22 +0900

    [Refactor] #38 버튼이 눌렸을때 작동하는 기능이 아닌 버튼 눌린 사실만 알 수 있도록 네이밍 변경

--- RefillStation/RefillStation/Application/Coordinator/HomeViewModelCoordinatorDelegate.swift
+++ RefillStation/RefillStation/Application/Coordinator/HomeViewModelCoordinatorDelegate.swift
@@ -8,5 +8,5 @@
 import Foundation
 
 protocol HomeViewModelCoordinatorDelegate: AnyObject {
-    func showRequestRegion()
+    func requestRegionButtonTapped()
 }
--- RefillStation/RefillStation/Application/Coordinator/RequestRegionViewModelCoordinatorDelegate.swift
+++ RefillStation/RefillStation/Application/Coordinator/RequestRegionViewModelCoordinatorDelegate.swift
@@ -8,5 +8,5 @@
 import Foundation
 
 protocol RequestRegionViewModelCoordinatorDelegate: AnyObject {
-    func requestRegionDidFinished()
+    func requestButtonTapped()
 }
