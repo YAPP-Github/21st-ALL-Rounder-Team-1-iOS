fe27d6003557d14aa266eef95adb25cce38dd494
kong <1018dbrud@gmail.com>
2022-12-20 16:54:44 +0900

    [Feat] $47 NetworkService 파일 생성

---
+++ RefillStation/RefillStation/Infrastructure/Network/NetworkService.swift
@@ -0,0 +1,21 @@
+//
+//  NetworkService.swift
+//  RefillStation
+//
+//  Created by kong on 2022/12/20.
+//
+
+import Foundation
+import Alamofire
+
+public enum NetworkError: Error {
+
+}
+
+protocol NetworkService {
+
+}
+
+final class BaseNetworkService: NetworkService {
+
+}
