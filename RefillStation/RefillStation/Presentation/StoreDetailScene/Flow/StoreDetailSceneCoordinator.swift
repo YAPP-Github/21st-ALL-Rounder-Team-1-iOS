ce6c0d3df8a3f891e7d147eb97a486db9b5069de
Neph <tony3322@naver.com>
2022-12-24 18:02:35 +0900

    [Feat] #50 StoreDetailSceneCoordinator의 start 메서드 구현

--- RefillStation/RefillStation/Presentation/StoreDetailScene/Flow/StoreDetailSceneCoordinator.swift
+++ RefillStation/RefillStation/Presentation/StoreDetailScene/Flow/StoreDetailSceneCoordinator.swift
@@ -5,16 +5,21 @@
 //  Created by 천수현 on 2022/12/24.
 //
 
-import Foundation
+import UIKit
 
 final class StoreDetailSceneCoordinator: Coordinator {
     var DIContainer: StoreDetailDIContainer
+    private weak var navigationController: UINavigationController?
 
-    init(storeDetailDIContainer: StoreDetailDIContainer) {
+    init(
+        navigationController: UINavigationController,
+        storeDetailDIContainer: StoreDetailDIContainer
+    ) {
         self.DIContainer = storeDetailDIContainer
     }
 
     func start() {
-
+        let storeDetailVC = DIContainer.makeStoreDetailViewController()
+        navigationController?.pushViewController(storeDetailVC, animated: true)
     }
 }
