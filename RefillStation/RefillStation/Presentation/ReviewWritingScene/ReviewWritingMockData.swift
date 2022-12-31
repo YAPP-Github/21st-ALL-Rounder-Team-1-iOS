9cbc37b78dd0ad4c1cd64ec674f816db26dcbafd
kong <1018dbrud@gmail.com>
2023-01-01 01:02:23 +0900

    [Chore] #56 tagTitle mockData 변경

--- RefillStation/RefillStation/Presentation/ReviewWritingScene/ReviewWritingMockData.swift
+++ RefillStation/RefillStation/Presentation/ReviewWritingScene/ReviewWritingMockData.swift
@@ -12,21 +12,21 @@ extension ReviewWritingViewController {
     func makeMockViewModel() -> DefaultTagReviewViewModel {
         let viewModel = DefaultTagReviewViewModel()
         viewModel.reviews = [
-            .init(tag: .init(image: UIImage(systemName: "zzz") ?? UIImage(), title: "점원이 친절해요"),
+            .init(tag: .init(image: UIImage(systemName: "zzz") ?? UIImage(), title: "포인트 적립이 가능해요"),
                   recommendedCount: 0),
             .init(tag: .init(image: UIImage(systemName: "zzz") ?? UIImage(), title: "품목이 다양해요"),
                   recommendedCount: 0),
+            .init(tag: .init(image: UIImage(systemName: "zzz") ?? UIImage(), title: "점원이 친절해요"),
+                  recommendedCount: 0),
+            .init(tag: .init(image: UIImage(systemName: "zzz") ?? UIImage(), title: "위치 접근성이 좋아요"),
+                  recommendedCount: 0),
+            .init(tag: .init(image: UIImage(systemName: "zzz") ?? UIImage(), title: "가격이 합리적이에요"),
+                  recommendedCount: 0),
+            .init(tag: .init(image: UIImage(systemName: "zzz") ?? UIImage(), title: "매장이 커요"),
+                  recommendedCount: 0),
             .init(tag: .init(image: UIImage(systemName: "zzz") ?? UIImage(), title: "매장이 청결해요"),
                   recommendedCount: 0),
-            .init(tag: .init(image: UIImage(systemName: "zzz") ?? UIImage(), title: "사장님이 맛있어요"),
-                  recommendedCount: 0),
-            .init(tag: .init(image: UIImage(systemName: "zzz") ?? UIImage(), title: "음식이 친절해요"),
-                  recommendedCount: 0),
-            .init(tag: .init(image: UIImage(systemName: "zzz") ?? UIImage(), title: "개발자가 죽어가요"),
-                  recommendedCount: 0),
-            .init(tag: .init(image: UIImage(systemName: "zzz") ?? UIImage(), title: "일정이 빡빡해요"),
-                  recommendedCount: 0),
-            .init(tag: .init(image: UIImage(systemName: "zzz") ?? UIImage(), title: "도움이 필요해요"),
+            .init(tag: .init(image: UIImage(systemName: "zzz") ?? UIImage(), title: "선택할 키워드가 없어요"),
                   recommendedCount: 0)
         ]
         return viewModel
