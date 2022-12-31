2b3230bcd830a9c8f2e7707f3f2838e4d53748ba
kong <1018dbrud@gmail.com>
2022-12-31 18:41:56 +0900

    [Feat] #52 RegisterReview Domain Layer

---
+++ RefillStation/RefillStation/Domain/Interfaces/Repositories/RegisterReviewRepositoryInterface.swift
@@ -0,0 +1,16 @@
+//
+//  RegisterReviewRepositoryInterface.swift
+//  RefillStation
+//
+//  Created by kong on 2022/12/16.
+//
+
+import Foundation
+
+protocol RegisterReviewRepositoryInterface {
+    func fetchTags(completion: @escaping(Result<[Tag], Error>) -> Void) -> Cancellable?
+    func registerReview(query: RegiserReviewRequestValue,
+                        completion: @escaping (Result<Never, Error>) -> Void) -> Cancellable?
+    func uploadReviewImage(query: UploadReviewImageRequestValue,
+                           completion: @escaping (Result<[String], Error>) -> Void) -> Cancellable?
+}
--- RefillStation/RefillStation/Domain/UseCases/UploadReviewImageUseCase.swift
+++ RefillStation/RefillStation/Domain/UseCases/UploadReviewImageUseCase.swift
@@ -5,4 +5,26 @@
 //  Created by kong on 2022/12/16.
 //
 
-import Foundation
+import UIKit
+
+struct UploadReviewImageRequestValue {
+    let images: [UIImage]
+}
+
+protocol UploadReviewImageUseCaseInterface {
+    func execute(requestValue: UploadReviewImageRequestValue,
+                 completion: @escaping (Result<[String], Error>) -> Void) -> Cancellable?
+}
+
+final class UploadReviewImageUseCase: UploadReviewImageUseCaseInterface {
+    private let registerReviewRepository: RegisterReviewRepositoryInterface
+
+    init(registerReviewRepository: RegisterReviewRepositoryInterface) {
+        self.registerReviewRepository = registerReviewRepository
+    }
+
+    func execute(requestValue: UploadReviewImageRequestValue,
+                 completion: @escaping (Result<[String], Error>) -> Void) -> Cancellable? {
+        return nil
+    }
+}
