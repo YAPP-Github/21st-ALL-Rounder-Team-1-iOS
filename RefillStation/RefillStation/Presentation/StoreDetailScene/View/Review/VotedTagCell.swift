cd45f21172ed0e084930ebf0e0afbcffb27b9fb4
Neph <tony3322@naver.com>
2023-01-06 13:59:07 +0900

    [Feat] #53 기존 VotedTag 내부 요소 삭제 및 새롭게 제작 할 요소들 추가

--- RefillStation/RefillStation/Presentation/StoreDetailScene/View/Review/VotedTagCell.swift
+++ RefillStation/RefillStation/Presentation/StoreDetailScene/View/Review/VotedTagCell.swift
@@ -14,169 +14,51 @@ final class VotedTagCell: UICollectionViewCell {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setUpContentView()
-        layout()
     }
-
-    private let firstClassBox = VotedTagReviewBox(tagClass: .first)
-    private let secondClassBox = VotedTagReviewBox(tagClass: .other)
-    private let thirdClassBox = VotedTagReviewBox(tagClass: .other)
-    private let forthClassBox = VotedTagReviewBox(tagClass: .other)
-
-    private let otherClassStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .horizontal
-        stackView.spacing = 10
-        stackView.distribution = .fillEqually
-        return stackView
-    }()
 
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
 
     func setUpContents(tagReviews: [TagReview]) {
-        let classes = [firstClassBox, secondClassBox, thirdClassBox, forthClassBox]
 
-        for index in 0..<tagReviews.count {
-            classes[index].setUpContents(tagReview: tagReviews[index])
-        }
-    }
-
-    private func setUpContentView() {
-        contentView.layer.cornerRadius = 5
-        contentView.clipsToBounds = true
-    }
-
-    private func layout() {
-        [firstClassBox, otherClassStackView].forEach {
-            contentView.addSubview($0)
-        }
-
-        firstClassBox.snp.makeConstraints { first in
-            first.leading.top.trailing.equalToSuperview()
-            first.height.equalToSuperview().multipliedBy(0.5)
-        }
-
-        otherClassStackView.snp.makeConstraints { stackView in
-            stackView.top.equalTo(firstClassBox.snp.bottom).offset(10)
-            stackView.leading.bottom.trailing.equalToSuperview()
-        }
-
-        [secondClassBox, thirdClassBox, forthClassBox].forEach {
-            otherClassStackView.addArrangedSubview($0)
-        }
     }
 }
 
-final class VotedTagReviewBox: UIView {
-
-    enum TagClass {
-        case first
-        case other
-    }
-
-    private let tagClass: TagClass
-
+fileprivate final class FirstRankView: UIView {
     private let tagImageView: UIImageView = {
         let imageView = UIImageView()
-        imageView.contentMode = .scaleAspectFill
         return imageView
     }()
 
-    private let tagTitleLabel: UILabel = {
+    private let rankLabel: UILabel = {
         let label = UILabel()
-        label.textColor = Asset.Colors.gray6.color
-        label.textAlignment = .center
-        label.numberOfLines = 0
         return label
     }()
 
-    private let voteCountLabel: UILabel = {
+    private let titleLabel: UILabel = {
         let label = UILabel()
-        label.textColor = Asset.Colors.primary2.color
         return label
     }()
 
-    init(tagClass: TagClass) {
-        self.tagClass = tagClass
-        super.init(frame: .zero)
-        setLabelFont()
-        setUpSelf()
-        layout()
-    }
+    private let divisionLine: UIView = {
+        let view = UIView(frame: .zero)
+        return view
+    }()
+}
 
-    required init?(coder: NSCoder) {
-        self.tagClass = .other
-        super.init(coder: coder)
-    }
+fileprivate final class OtherRankView: UIView {
+    private let rankLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    private let tagImageView: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
 
-    func setUpContents(tagReview: TagReview) {
-        tagImageView.image = tagReview.tag.image
-        tagTitleLabel.text = tagReview.tag.title
-        voteCountLabel.text = "\(tagReview.recommendedCount)"
-    }
-
-    private func setLabelFont() {
-        [tagTitleLabel, voteCountLabel].forEach {
-            if self.tagClass == .first {
-                $0.font = UIFont.font(style: .titleSmall)
-            } else {
-                $0.font = .systemFont(ofSize: 12)
-            }
-        }
-    }
-
-    private func setUpSelf() {
-        layer.cornerRadius = 6
-        clipsToBounds = true
-        backgroundColor = Asset.Colors.gray1.color
-    }
-
-    private func layout() {
-        [tagImageView, tagTitleLabel, voteCountLabel].forEach {
-            addSubview($0)
-        }
-
-        tagImageView.snp.makeConstraints { image in
-            image.centerX.equalToSuperview()
-        }
-
-        addConstraintsWithTagClassOption()
-    }
-
-    private func addConstraintsWithTagClassOption() {
-        if tagClass == .first {
-            tagTitleLabel.snp.remakeConstraints { title in
-                title.top.equalTo(tagImageView.snp.bottom).offset(10)
-                title.centerX.equalToSuperview()
-            }
-
-            voteCountLabel.snp.makeConstraints { count in
-                count.top.equalTo(tagTitleLabel.snp.top)
-                count.leading.equalTo(tagTitleLabel.snp.trailing).offset(5)
-            }
-
-            tagImageView.snp.makeConstraints { image in
-                image.centerY.equalToSuperview().multipliedBy(0.8)
-                image.width.height.equalTo(50)
-            }
-
-        } else {
-            tagTitleLabel.snp.makeConstraints { title in
-                title.top.equalTo(tagImageView.snp.bottom).offset(10)
-                title.leading.trailing.equalToSuperview().inset(10)
-            }
-
-            voteCountLabel.snp.makeConstraints { count in
-                count.top.equalTo(tagTitleLabel.snp.bottom).offset(5)
-                count.centerX.equalToSuperview()
-            }
-
-            tagImageView.snp.makeConstraints { image in
-                image.centerY.equalToSuperview().multipliedBy(0.7)
-                image.width.height.equalTo(20)
-            }
-        }
-    }
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
 }
