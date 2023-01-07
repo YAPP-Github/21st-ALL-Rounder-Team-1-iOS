7ec4aa8994c305f9a5569217943fa58280687436
Neph <tony3322@naver.com>
2023-01-07 14:06:16 +0900

    [Chore] #53 삭제되지 않고있던 Asset파일들 삭제

--- RefillStation/RefillStation/Resources/Assets+Generated.swift
+++ RefillStation/RefillStation/Resources/Assets+Generated.swift
@@ -51,20 +51,10 @@ internal enum Asset {
       internal static let almaengShop = ImageAsset(name: "almaengShop")
       internal static let arganShampoo = ImageAsset(name: "arganShampoo")
       internal static let bodyWash = ImageAsset(name: "bodyWash")
-      internal static let bubbles = ImageAsset(name: "bubbles")
-      internal static let creditCard = ImageAsset(name: "credit-card")
       internal static let earthShop = ImageAsset(name: "earthShop")
       internal static let eco = ImageAsset(name: "eco")
       internal static let purifyingShampoo = ImageAsset(name: "purifyingShampoo")
-      internal static let review3 = ImageAsset(name: "review(3)")
-      internal static let review4 = ImageAsset(name: "review(4)")
       internal static let scrupShampoo = ImageAsset(name: "scrupShampoo")
-      internal static let shop = ImageAsset(name: "shop")
-      internal static let smilingFace = ImageAsset(name: "smiling-face")
-      internal static let user2 = ImageAsset(name: "user (2)")
-      internal static let user1 = ImageAsset(name: "user(1)")
-      internal static let user3 = ImageAsset(name: "user(3)")
-      internal static let user4 = ImageAsset(name: "user(4)")
       internal static let youngGram = ImageAsset(name: "youngGram")
     }
     internal static let iconAlbum = ImageAsset(name: "icon_Album")
@@ -88,6 +78,7 @@ internal enum Asset {
     internal static let iconSocialloginNaver = ImageAsset(name: "icon_sociallogin_naver")
     internal static let iconThumbsup = ImageAsset(name: "icon_thumbsup")
     internal static let iconWrite = ImageAsset(name: "icon_write")
+    internal static let profileImageGroup = ImageAsset(name: "profileImageGroup")
     internal static let regionImage = ImageAsset(name: "regionImage")
   }
 }
