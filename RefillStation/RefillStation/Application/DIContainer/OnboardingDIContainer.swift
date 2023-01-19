b9e7cd9d963e75fee53b56f4f846ef10ae93c38c
Neph <tony3322@naver.com>
2023-01-19 16:45:43 +0900

    [Feat] Onboarding DIContainer 필요한 메서드 틀 제작

--- RefillStation/RefillStation/Application/DIContainer/OnboardingDIContainer.swift
+++ RefillStation/RefillStation/Application/DIContainer/OnboardingDIContainer.swift
@@ -6,7 +6,67 @@
 //
 
 import Foundation
+import UIKit
 
 final class OnboardingDIContainer: DIContainer {
-    
+    private let navigationController: UINavigationController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    // MARK: - Coordinator
+    func makeOnboardingCoordinator() {
+
+    }
+
+    // MARK: - Onboarding
+    func makeOnboardingViewController() {
+
+    }
+
+    func makeOnboardingViewModel() {
+
+    }
+
+    // MARK: - Login
+    func makeLoginViewController() {
+
+    }
+
+    func makeLoginViewModel() {
+
+    }
+
+    // MARK: - Terms Permission
+    func makeTermsPermissionViewController() {
+
+    }
+
+    func makeTermsPermissionViewModel() {
+
+    }
+
+    // MARK: - Terms Detail
+    func makeTermsDetailViewController() {
+
+    }
+
+    func makeTermsDetailViewModel() {
+
+    }
+
+    // MARK: - Nickname
+    func makeNicknameViewController() {
+
+    }
+
+    func makeNicknameViewModel() {
+
+    }
+
+    // MARK: - Location Permission
+    func makeLocationPermissionViewController() {
+
+    }
 }
