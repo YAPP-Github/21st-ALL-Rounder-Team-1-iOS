bb6042764c4b5f430dd1483a21a9785a926a1131
Neph <tony3322@naver.com>
2023-01-20 15:30:30 +0900

    [Feat] #78 MyPage 뷰컨, 뷰모델 생성

---
+++ RefillStation/RefillStation/Presentation/MyPageScene/MyPageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MyPageViewController.swift
+//  RefillStation
+//
+//  Created by 천수현 on 2023/01/20.
+//
+
+import UIKit
+
+final class MyPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ RefillStation/RefillStation/Presentation/MyPageScene/MyPageViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MyPageViewModel.swift
+//  RefillStation
+//
+//  Created by 천수현 on 2023/01/20.
+//
+
+import Foundation
+
+final class MyPageViewModel {
+    
+}
