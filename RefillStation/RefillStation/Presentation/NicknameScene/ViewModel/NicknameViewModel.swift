9070f8a5c2f5f9c90cf1753c335b10f8d5156f7c
kong <1018dbrud@gmail.com>
2023-01-23 01:32:18 +0900

    [Feat] #73 NicknameViewModel 생성

---
+++ RefillStation/RefillStation/Presentation/NicknameScene/ViewModel/NicknameViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  NicknameViewModel.swift
+//  RefillStation
+//
+//  Created by kong on 2023/01/22.
+//
+
+import Foundation
+
+final class NicknameViewModel {
+    var profileImage: String?
+    var randomNickname = "냥냥이에오123"
+    var userNickname = "kong"
+    var isVaild = true
+}
