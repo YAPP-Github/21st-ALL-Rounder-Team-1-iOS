236d1fc6c3cd41e4314dce9b806e8ff04bfbc982
Neph <tony3322@naver.com>
2023-01-24 16:44:15 +0900

    [Feat] #83 신고 팝업 viewModel 제작

---
+++ RefillStation/RefillStation/Presentation/StoreDetailScene/View/Review/ReviewReportPopUpViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  ReviewReportPopUpViewModel.swift
+//  RefillStation
+//
+//  Created by 천수현 on 2023/01/24.
+//
+
+import Foundation
+
+final class ReviewReportPopUpViewModel {
+    private let reportedUserId: Int
+
+    init(reportedUserId: Int) {
+        self.reportedUserId = reportedUserId
+    }
+
+    func reportButtonTapped(completion: @escaping (() -> Void)) {
+
+    }
+
+    private func report() {
+
+    }
+}
