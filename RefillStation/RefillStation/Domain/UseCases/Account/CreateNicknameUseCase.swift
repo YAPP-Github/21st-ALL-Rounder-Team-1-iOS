9e78f4c1258c21822879dc8d891b3fdb27a4b4e6
Neph <tony3322@naver.com>
2023-01-25 23:55:23 +0900

    [Feat] #87 CreateNickname, CS UseCase 제작

--- RefillStation/RefillStation/Domain/UseCases/Account/CreateNicknameUseCase.swift
+++ RefillStation/RefillStation/Domain/UseCases/Account/CreateNicknameUseCase.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+protocol CreateNicknameUseCaseInterface {
+    func execute(completion: @escaping (Result<String, Error>) -> Void) -> Cancellable?
+}
+
+final class CreateNicknameUseCase: CreateNicknameUseCaseInterface {
+    private let accountRepository: AccountRepositoryInterface
+
+    init(accountRepository: AccountRepositoryInterface) {
+        self.accountRepository = accountRepository
+    }
+
+    func execute(completion: @escaping (Result<String, Error>) -> Void) -> Cancellable? {
+        return nil
+    }
+}
--- RefillStation/RefillStation/Domain/UseCases/CustomerSatisfaction/ReportUserUseCase.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ReportUserUseCase.swift
-//  RefillStation
-//
-//  Created by 천수현 on 2023/01/25.
-//
-
-import Foundation
