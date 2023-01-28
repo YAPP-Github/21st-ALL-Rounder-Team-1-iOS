61ed40914019731b464c688124183ab3db44b215
Neph <tony3322@naver.com>
2023-01-28 20:26:14 +0900

    [Feat] #89 OperationNoticeCell 제작

---
+++ RefillStation/RefillStation/Presentation/StoreDetailScene/View/OperationInfo/OperationNoticeCell.swift
@@ -0,0 +1,49 @@
+//
+//  OperationNoticeCell.swift
+//  RefillStation
+//
+//  Created by 천수현 on 2023/01/28.
+//
+
+import UIKit
+
+final class OperationNoticeCell: UICollectionViewCell {
+
+    static let reuseIdentifier = String(describing: OperationNoticeCell.self)
+
+    private let noticeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "매장 상황에 따라 운영시간이 달라질 수 있어요."
+        label.font = .font(style: .bodySmall)
+        label.textColor = Asset.Colors.gray5.color
+        return label
+    }()
+
+    private lazy var noticeView: UIView = {
+        let noticeView = UIView()
+        noticeView.backgroundColor = Asset.Colors.primary1.color
+        noticeView.layer.cornerRadius = 4
+        noticeView.addSubview(noticeLabel)
+        noticeLabel.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(26)
+            $0.top.bottom.equalToSuperview().inset(12)
+        }
+        return noticeView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        layout()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    private func layout() {
+        contentView.addSubview(noticeView)
+        noticeView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
