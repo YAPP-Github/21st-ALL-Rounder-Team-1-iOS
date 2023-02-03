c9d66719f52d5cf693fbfae73637408226c1584d
kong <1018dbrud@gmail.com>
2023-02-04 04:50:15 +0900

    [Refactor] #111 지역 추가 신청 헤더에 현재 위치 뷰만 분리

--- RefillStation/RefillStation/Presentation/HomeScene/View/RegionRequestHeaderView.swift
+++ RefillStation/RefillStation/Presentation/HomeScene/View/RegionRequestHeaderView.swift
@@ -11,9 +11,9 @@ import SnapKit
 final class RegionRequestHeaderView: UICollectionReusableView {
 
     static let reuseIdentifier = String(describing: RegionRequestHeaderView.self)
-    var moveToRegionRequest: (() -> Void)?
 
     // MARK: - UIComponents
+
     private let locationIcon: UIImageView = {
         let imageView = UIImageView()
         imageView.image = Asset.Images.iconPosition.image
@@ -24,44 +24,6 @@ final class RegionRequestHeaderView: UICollectionReusableView {
         let label = UILabel()
         label.font = .font(style: .captionLarge)
         label.textColor = Asset.Colors.gray5.color
-        label.text = "서울 마포구 월드컵로 212"
-        return label
-    }()
-    private let guidanceLabel: UILabel = {
-        let label = UILabel()
-        label.text = "현재 위치하고 계신 곳은\n아직 서비스 지역이 아니에요😭"
-        label.font = .font(style: .titleMedium)
-        label.textAlignment = .center
-        label.numberOfLines = 0
-        return label
-    }()
-    private let imageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = Asset.Images.imgServiceArea.image
-        imageView.contentMode = .scaleAspectFill
-        return imageView
-    }()
-    private lazy var requestButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("서비스 지역 신청하러 가기", for: .normal)
-        button.backgroundColor = Asset.Colors.primary3.color
-        button.setTitleColor(Asset.Colors.primary10.color, for: .normal)
-        button.titleLabel?.font = .font(style: .buttonLarge)
-        button.addTarget(self, action: #selector(requestButtonDidTap), for: .touchUpInside)
-        button.layer.cornerRadius = 6
-        return button
-    }()
-
-    private let dividerView: UIView = {
-        let view = UIView()
-        view.backgroundColor = Asset.Colors.gray1.color
-        return view
-    }()
-
-    private let headerTitleLabel: UILabel = {
-        let label = UILabel()
-        label.text = "서비스 지역 매장 둘러보기"
-        label.font = .font(style: .titleMedium)
         return label
     }()
 
@@ -76,11 +38,13 @@ final class RegionRequestHeaderView: UICollectionReusableView {
     }
 
     // MARK: - Default Setting Methods
+    func setUpView(address: String) {
+        currentLocationLabel.text = address
+    }
     private func layout() {
-        [locationIcon, currentLocationLabel, guidanceLabel,
-         imageView, requestButton, dividerView, headerTitleLabel].forEach { addSubview($0) }
+        [locationIcon, currentLocationLabel].forEach { addSubview($0) }
         locationIcon.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(66)
+            $0.top.bottom.equalToSuperview().inset(8)
             $0.leading.equalToSuperview().inset(16)
             $0.width.height.equalTo(24)
         }
@@ -88,34 +52,5 @@ final class RegionRequestHeaderView: UICollectionReusableView {
             $0.leading.equalTo(locationIcon.snp.trailing).offset(6)
             $0.centerY.equalTo(locationIcon)
         }
-        guidanceLabel.snp.makeConstraints {
-            $0.top.equalTo(locationIcon.snp.bottom).offset(24)
-            $0.centerX.equalToSuperview()
-        }
-        imageView.snp.makeConstraints {
-            $0.top.equalTo(guidanceLabel.snp.bottom).offset(8)
-            $0.height.equalTo(134)
-            $0.centerX.equalToSuperview()
-        }
-        requestButton.snp.makeConstraints {
-            $0.top.equalTo(imageView.snp.bottom).offset(6)
-            $0.leading.trailing.equalToSuperview().inset(48)
-            $0.height.equalTo(40)
-        }
-        dividerView.snp.makeConstraints {
-            $0.top.equalTo(requestButton.snp.bottom).offset(20)
-            $0.height.equalTo(8)
-            $0.leading.trailing.equalToSuperview()
-        }
-        headerTitleLabel.snp.makeConstraints {
-            $0.top.equalTo(dividerView.snp.bottom).offset(20)
-            $0.leading.trailing.equalToSuperview().inset(16)
-            $0.bottom.equalToSuperview()
-        }
-    }
-
-    // MARK: - Function
-    @objc private func requestButtonDidTap() {
-        moveToRegionRequest?()
     }
 }
