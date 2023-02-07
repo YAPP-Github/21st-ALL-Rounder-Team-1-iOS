59c81fee6098ca7e714ec07a9d0fea0c83a85202
Neph <tony3322@naver.com>
2023-02-07 16:56:44 +0900

    [Feat] #123 AWSS3 서비스 계층 제작

--- RefillStation/RefillStation/Infrastructure/AWSS3Service.swift
+++ RefillStation/RefillStation/Infrastructure/AWSS3Service.swift
@@ -7,54 +7,86 @@
 
 import UIKit
 import AWSS3
-import ClientRuntime
-import AWSClientRuntime
+import AWSCore
 
 enum AWSS3Error: Error {
     case imageDataConvertFailed
 }
 
 public class AWSS3Service {
-    enum BucketType {
+    enum Bucket {
         case review
         case store
 
-        var bucketName: String {
-            return "pump-img-bucket"
-        }
+        static let bucketName = "pump-img-bucket"
+        static let baseURL = "https://pump-img-bucket.s3.ap-northeast-2.amazonaws.com"
 
-        var URL: String {
+        var name: String {
             switch self {
             case .review:
-                return "https://pump-img-bucket.s3.ap-northeast-2.amazonaws.com/review"
+                return "review"
             case .store:
-                return "https://pump-img-bucket.s3.ap-northeast-2.amazonaws.com/store"
+                return "store"
             }
         }
     }
 
-    let client: S3Client
-
-    public init() async {
-        do {
-            client = try await S3Client()
-        } catch {
-            fatalError()
-        }
+    private var path: String {
+        guard let path = Bundle.main.path(forResource: "AWSAccessKey", ofType: "plist") else { return "" }
+        return path
     }
 
-    func uploadImage(type: BucketType, key: String, image: UIImage?) async throws -> String {
-        guard let imageData = image?.jpegData(compressionQuality: 1) else { throw AWSS3Error.imageDataConvertFailed }
-        let dataStream = ByteStream.from(data: imageData)
+    private var accessKey: String {
+        guard let dictionary = NSDictionary(contentsOfFile: path),
+              let key = dictionary["AWS_ACCESS_KEY"] as? String else { return "" }
+        return key
+    }
 
-        let input = PutObjectInput(
-            body: dataStream,
-            bucket: type.bucketName,
-            key: key
+    private var secretKey: String {
+        guard let dictionary = NSDictionary(contentsOfFile: path),
+              let key = dictionary["AWS_SECRET_KEY"] as? String else { return "" }
+        return key
+    }
+
+    private var region: String {
+        guard let dictionary = NSDictionary(contentsOfFile: path),
+              let key = dictionary["AWS_REGION"] as? String else { return "" }
+        return key
+    }
+
+    static let shared = AWSS3Service()
+
+    init() {
+        let credentialsProvider = AWSStaticCredentialsProvider(accessKey: accessKey, secretKey: secretKey)
+        let configuration = AWSServiceConfiguration(region: .APNortheast2, credentialsProvider: credentialsProvider)
+        AWSServiceManager.default().defaultServiceConfiguration = configuration
+
+        let tuConf = AWSS3TransferUtilityConfiguration()
+        tuConf.isAccelerateModeEnabled = false
+
+        AWSS3TransferUtility.register(
+            with: configuration!,
+            transferUtilityConfiguration: tuConf,
+            forKey: "utility-key"
         )
+    }
 
-        _ = try await client.putObject(input: input)
+    func upload(type: Bucket, image: UIImage?, completion: @escaping (Result<String, Error>) -> Void) {
+        guard let transferUtility = AWSS3TransferUtility.s3TransferUtility(forKey: "utility-key"),
+              let imageData = image?.jpegData(compressionQuality: 1) else { return }
 
-        return type.URL + "/\(key)"
+        let id = UUID().uuidString
+        let fileKey = "\(type.name)/\(id).jpeg"
+
+        transferUtility.uploadData(
+            imageData,
+            bucket: Bucket.bucketName,
+            key: fileKey,
+            contentType: "image/jpeg",
+            expression: nil) { task, error in
+                print(error)
+                guard error == nil else { return }
+                completion(.success(Bucket.baseURL + "/" + fileKey))
+            }
     }
 }
