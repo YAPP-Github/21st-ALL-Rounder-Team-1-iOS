ec85c3afafcff5287fe5de6fb50d3d14cfff2ef0
Neph <tony3322@naver.com>
2023-02-11 01:51:11 +0900

    [Feat] #144 Async CS 레포, 인터페이스 제작

---
+++ RefillStation/RefillStation/Data/Repositories/AsyncCustomerSatisfactionRepository.swift
@@ -0,0 +1,43 @@
+//
+//  AsyncCustomerSatisfactionRepository.swift
+//  RefillStation
+//
+//  Created by 천수현 on 2023/02/11.
+//
+
+import Foundation
+
+final class AysncCustomerSatisfactionRepository: AsyncCustomerSatisfactionRepositoryInterface {
+    private let networkService: NetworkServiceInterface
+    private let userInfoRepository: AsyncUserInfoRepositoryInterface
+
+    init(
+        networkService: NetworkServiceInterface = NetworkService.shared,
+        userInfoRepository: AsyncUserInfoRepository = AsyncUserInfoRepository()
+    ) {
+        self.networkService = networkService
+        self.userInfoRepository = userInfoRepository
+    }
+
+    func upload(requestValue: CustomerSatisfactionRequestValue) async throws {
+        var urlComponents = URLComponents(string: networkService.baseURL)
+        urlComponents?.path = "/api/cs"
+        let userId = requestValue.type == .reportUser ? requestValue.userId
+        : try await userInfoRepository.fetchProfile().id
+
+        let dto = CustomerSatisfactionDTO(
+            userId: userId,
+            type: requestValue.type.rawValue,
+            content: requestValue.content
+        )
+        guard let requestBody = try? JSONEncoder().encode(dto) else {
+            throw RepositoryError.requestParseFailed
+        }
+
+        guard let request = urlComponents?.toURLRequest(method: .post, httpBody: requestBody) else {
+            throw RepositoryError.urlParseFailed
+        }
+
+        let _: CustomerSatisfactionResponseDTO = try await networkService.dataTask(request: request)
+    }
+}
