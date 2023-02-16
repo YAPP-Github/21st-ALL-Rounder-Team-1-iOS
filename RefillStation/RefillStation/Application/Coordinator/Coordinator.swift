589067b49477ab229d9fe59460a6909cb27c2617
Neph <tony3322@naver.com>
2023-02-16 14:49:50 +0900

    [Feat] #180 로그인페이지 띄우는 공통로직 제작

--- RefillStation/RefillStation/Application/Coordinator/Coordinator.swift
+++ RefillStation/RefillStation/Application/Coordinator/Coordinator.swift
@@ -5,11 +5,24 @@
 //  Created by 천수현 on 2022/12/16.
 //
 
-import Foundation
+import UIKit
 
 protocol Coordinator: AnyObject {
     associatedtype DIContainerProtocol: DIContainer
     var DIContainer: DIContainerProtocol { get }
 
     func start()
+}
+
+extension Coordinator {
+    func showLoginPage() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let navigationController = UINavigationController()
+        let onboardingDIContainer = OnboardingDIContainer(navigationController: navigationController,
+                                                          window: appDelegate.window)
+        let onboardingCoordinator = onboardingDIContainer.makeOnboardingCoordinator()
+        let loginViewController = onboardingDIContainer.makeLoginViewController()
+        loginViewController.coordinator = onboardingCoordinator
+        appDelegate.window?.rootViewController = loginViewController
+    }
 }
