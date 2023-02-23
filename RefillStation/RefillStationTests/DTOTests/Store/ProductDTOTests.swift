4caeaa5be8ea6e622cbbb860d4be2d4b00403b93
Neph <tony3322@naver.com>
2023-02-23 12:37:22 +0900

    [Test] #179 DTO-Store-store, product 테스트

---
+++ RefillStation/RefillStationTests/DTOTests/Store/ProductDTOTests.swift
@@ -0,0 +1,43 @@
+//
+//  ProductDTOTests.swift
+//  RefillStationTests
+//
+//  Created by 천수현 on 2023/02/23.
+//
+
+import XCTest
+@testable import RefillStation
+
+final class ProductDTOTests: XCTestCase {
+    var fullContentUnit: ProductDTO!
+    var minimumContentUnit: ProductDTO!
+
+    override func setUpWithError() throws {
+        fullContentUnit = ProductDTO(createdAt: "", modifiedAt: "", id: 0, storeId: 0, title: "title", price: 10, unit: "mg", brand: "brand", category: "샴푸", imgPath: "imgPath", isHided: true, isReady: true)
+
+        minimumContentUnit = ProductDTO(createdAt: nil, modifiedAt: nil, id: nil, storeId: nil, title: nil, price: nil, unit: nil, brand: nil, category: nil, imgPath: nil, isHided: nil, isReady: nil)
+    }
+
+    override func tearDownWithError() throws {
+        fullContentUnit = nil
+        minimumContentUnit = nil
+    }
+
+    func test_모든_필드가_채워진_LoginDTO의_toDomain_메서드를_호출하면_모든_필드가_채워진_OAuthLoginResponseValue를_반환하는지() {
+        // given
+        // when
+        let domainResult = fullContentUnit.toDomain()
+        // then
+        let expectationResult = Product(name: "title", brand: "brand", measurement: "mg", price: 10, imageURL: "imgPath", category: .init(title: "샴푸"))
+        XCTAssertEqual(domainResult, expectationResult)
+    }
+
+    func test_최소한의_필드만_채워진_LoginDTO의_toDomain_메서드를_호출하면_기본값으로_채워진_OAuthLoginResponseValue를_반환하는지() {
+        // given
+        // when
+        let domainResult = minimumContentUnit.toDomain()
+        // then
+        let expectationResult = Product(name: "", brand: "", measurement: "", price: 0, imageURL: "", category: .init(title: ""))
+        XCTAssertEqual(domainResult, expectationResult)
+    }
+}
---
+++ RefillStation/RefillStationTests/DTOTests/Store/StoreDTOTests.swift
@@ -0,0 +1,44 @@
+//
+//  StoreDTOTests.swift
+//  RefillStationTests
+//
+//  Created by 천수현 on 2023/02/23.
+//
+
+import XCTest
+@testable import RefillStation
+
+final class StoreDTOTests: XCTestCase {
+    var fullContentUnit: StoreDTO!
+    var minimumContentUnit: StoreDTO!
+
+    override func setUpWithError() throws {
+        fullContentUnit = StoreDTO(id: 0, userId: 0, name: "name", status: "", longitude: "", latitude: "", businessHour: [.init(day: "월", time: "test time")], notice: "", address: "address", instaAccount: "instaAccount", callNumber: "", registrationNumber: "", isReady: true, distance: "10", imgStores: [.init(id: 0, storeId: 0, path: "")], storeRefillGuides: [.init(createdAt: "", modifiedAt: "", id: 0, storeId: 0, imgPath: "", removedAt: "")])
+
+        minimumContentUnit = StoreDTO(id: nil, userId: nil, name: nil, status: nil, longitude: nil, latitude: nil, businessHour: nil, notice: nil, address: nil, instaAccount: nil, callNumber: nil, registrationNumber: nil, isReady: nil, distance: nil, imgStores: [], storeRefillGuides: [])
+    }
+
+    override func tearDownWithError() throws {
+        fullContentUnit = nil
+        minimumContentUnit = nil
+    }
+
+    func test_모든_필드가_채워진_LoginDTO의_toDomain_메서드를_호출하면_모든_필드가_채워진_OAuthLoginResponseValue를_반환하는지() {
+        // given
+        // when
+        let domainResult = fullContentUnit.toDomain()
+        // then
+        let expectationResult = Store(storeId: 0, name: "name", address: "address", distance: 10, phoneNumber: "", snsAddress: "instaAccount", didUserRecommended: false, recommendedCount: 0, imageURL: [""], businessHour: [.init(day: .mon, time: "test time")], notice: "", storeRefillGuideImagePaths: [""])
+        XCTAssertEqual(domainResult, expectationResult)
+    }
+
+    func test_최소한의_필드만_채워진_LoginDTO의_toDomain_메서드를_호출하면_기본값으로_채워진_OAuthLoginResponseValue를_반환하는지() {
+        // given
+        // when
+        let domainResult = minimumContentUnit.toDomain()
+        // then
+        let expectationResult = Store(storeId: 0, name: "", address: "", distance: 0, phoneNumber: "", snsAddress: "", didUserRecommended: false, recommendedCount: 0, imageURL: [], businessHour: [], notice: "", storeRefillGuideImagePaths: [])
+        XCTAssertEqual(domainResult, expectationResult)
+    }
+}
+
