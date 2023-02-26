adaa5cfec1a0247077223edf60ef99b59db6fd57
kong <1018dbrud@gmail.com>
2023-02-27 03:46:13 +0900

    [Feat] #211 KeychainManagerInterface 및 Mock 추가

---
+++ RefillStation/RefillStationTests/RepositoryTests/RegisterReview/MockKeychainManager.swift
@@ -0,0 +1,27 @@
+//
+//  MockKeychainManager.swift
+//  RefillStationTests
+//
+//  Created by kong on 2023/02/27.
+//
+
+import Foundation
+@testable import RefillStation
+
+class MockKeychainManager: KeychainManagerInterface {
+    func addItem(key: Any, value: Any) -> Bool {
+        true
+    }
+    func getItem(key: Any) -> Any? {
+        nil
+    }
+    func updateItem(key: Any, value: Any) -> Bool {
+        true
+    }
+    func deleteItem(key: String) -> Bool {
+        true
+    }
+    func deleteUserToken() -> Result<Void, Error> {
+        .success(())
+    }
+}
